bd40a0151bffee3d6d1b21b165aa14ecb3832cc8
hipsu2020 <75387975+hipsu2020@users.noreply.github.com>
2021-03-06 16:03:08 +0200

    new forecast API and fahrenheit convertion for forecast

--- src/index.js
+++ src/index.js
@@ -76,10 +76,10 @@ function searchCoordinates(position) {
       `${apiUrl}${apiUrlCoordsToday}lat=${currentLat}&lon=${currentLon}&units=${units}&appid=${apiKey}`
     )
     .then(showWeather);
-let apiUrlCoordsForecast = `forecast?`;
+let apiUrlCoordsForecast = `onecall?`;
   axios
     .get(
-      `${apiUrl}${apiUrlCoordsForecast}lat=${currentLat}&lon=${currentLon}&units=${units}&appid=${apiKey}`
+      `${apiUrl}${apiUrlCoordsForecast}lat=${currentLat}&lon=${currentLon}&units=${units}&exclude=hourly,minutely&appid=${apiKey}`
     ).then(showForecast); 
 }
 
@@ -91,11 +91,28 @@ function locateCoordinates() {
 let button = document.querySelector("#locateButton");
 button.addEventListener("click", locateCoordinates);
 
+let currentTempElement = document.querySelector("#currentTemperature");
+let currentTempMaxElement = document.querySelector("#currentTempMax");
+let currentTempMinElement = document.querySelector("#currentTempMin");
+let firstDayMaxElement = document.querySelector("#firstMax");
+let firstDayMinElement = document.querySelector("#firstMin");
+let firstWeatherIcon = document.querySelector("#firstWeatherIcon");
+let secondDayMaxElement = document.querySelector("#secondMax");
+let secondDayMinElement = document.querySelector("#secondMin");
+let secondWeatherIcon = document.querySelector("#secondWeatherIcon");
+let thirdDayMaxElement = document.querySelector("#thirdMax");
+let thirdDayMinElement = document.querySelector("#thirdMin");
+let thirdWeatherIcon = document.querySelector("#thirdWeatherIcon");
+let fourthDayMaxElement = document.querySelector("#fourthMax");
+let fourthDayMinElement = document.querySelector("#fourthMin");
+let fourthWeatherIcon = document.querySelector("#fourthWeatherIcon");
+let fifthDayMaxElement = document.querySelector("#fifthMax");
+let fifthDayMinElement = document.querySelector("#fifthMin");
+let fifthWeatherIcon = document.querySelector("#fifthWeatherIcon");
+
 //to show the current weather
 function showWeather(response) {
-  let currentTempElement = document.querySelector("#currentTemperature");
-  let currentTempMaxElement = document.querySelector("#currentTempMax");
-  let currentTempMinElement = document.querySelector("#currentTempMin");
+
   const weatherMainly = document.querySelector("#weatherMainly");
   let weatherIconCodeToday = response.data.weather[0].icon;
   let weatherIconToday = document.querySelector("#currentWeatherIcon");
@@ -123,53 +140,55 @@ let thirdDay = document.querySelector("#thirdDay");
 let fourthDay = document.querySelector("#fourthDay");
 let fifthDay = document.querySelector("#fifthDay");
 
+
+
 //For weather forecast
 function showForecast(response) {
   console.log(response);
-  console.log(response.data.list[1].main.temp_max);
-  console.log(response.data.list[1].main.temp_min);
-  console.log(response.data.list[1].weather[0].main);
-  console.log(response.data.list[1].weather[0].icon);
-  let firstDayMax = document.querySelector("#firstMax");
-  let firstDayMin = document.querySelector("#firstMin");
-  let firstWeatherIconCode = response.data.list[1].weather[0].icon;
-  let firstWeatherIcon = document.querySelector("#firstWeatherIcon");
-  firstDayMax.innerHTML = "Max: " + Math.round(response.data.list[1].main.temp_max);
-  firstDayMin.innerHTML = "Min: " + Math.round(response.data.list[1].main.temp_min);
+
+  let firstWeatherIconCode = response.data.daily[0].weather[0].icon;
+  let secondWeatherIconCode = response.data.daily[1].weather[0].icon;
+  let thirdWeatherIconCode = response.data.daily[2].weather[0].icon;
+  let fourthWeatherIconCode = response.data.daily[3].weather[0].icon;
+  let fifthWeatherIconCode = response.data.daily[4].weather[0].icon;
+  
+
+  firstDayCelsiusTempMax = (response.data.daily[0].temp.max);
+  firstDayCelsiusTempMin = (response.data.daily[0].temp.min);
+  firstDayMaxElement.innerHTML = "Max: " + Math.round(firstDayCelsiusTempMax);
+  firstDayMinElement.innerHTML = "Min: " + Math.round(firstDayCelsiusTempMin);
   firstWeatherIcon.setAttribute("src",`http://openweathermap.org/img/wn/${firstWeatherIconCode}@2x.png`);
-  firstWeatherIcon.setAttribute("alt", response.data.list[1].weather[0].main);
-  let secondDayMax = document.querySelector("#secondMax");
-  let secondDayMin = document.querySelector("#secondMin");
-  let secondWeatherIconCode = response.data.list[2].weather[0].icon;
-  let secondWeatherIcon = document.querySelector("#secondWeatherIcon");
-  secondDayMax.innerHTML = "Max: " + Math.round(response.data.list[2].main.temp_max);
-  secondDayMin.innerHTML = "Min: " + Math.round(response.data.list[2].main.temp_min);
+  firstWeatherIcon.setAttribute("alt", response.data.daily[0].weather[0].description);
+
+  secondDayCelsiusTempMax = (response.data.daily[1].temp.max);
+  secondDayCelsiusTempMin = (response.data.daily[1].temp.min);
+  secondDayMaxElement.innerHTML = "Max: " + Math.round(secondDayCelsiusTempMax);
+  secondDayMinElement.innerHTML = "Min: " + Math.round(secondDayCelsiusTempMin);
   secondWeatherIcon.setAttribute("src",`http://openweathermap.org/img/wn/${secondWeatherIconCode}@2x.png`);
-  secondWeatherIcon.setAttribute("alt", response.data.list[2].weather[0].main);
-  let thirdDayMax = document.querySelector("#thirdMax");
-  let thirdDayMin = document.querySelector("#thirdMin");
-  let thirdWeatherIconCode = response.data.list[3].weather[0].icon;
-  let thirdWeatherIcon = document.querySelector("#thirdWeatherIcon");
-  thirdDayMax.innerHTML = "Max: " + Math.round(response.data.list[3].main.temp_max);
-  thirdDayMin.innerHTML = "Min: " + Math.round(response.data.list[3].main.temp_min);
+  secondWeatherIcon.setAttribute("alt", response.data.daily[1].weather[0].description);
+  
+  
+  thirdDayCelsiusTempMax = (response.data.daily[2].temp.max);
+  thirdDayCelsiusTempMin = (response.data.daily[2].temp.min);
+  thirdDayMaxElement.innerHTML = "Max: " + Math.round(thirdDayCelsiusTempMax);
+  thirdDayMinElement.innerHTML = "Min: " + Math.round(thirdDayCelsiusTempMin);
   thirdWeatherIcon.setAttribute("src",`http://openweathermap.org/img/wn/${thirdWeatherIconCode}@2x.png`);
-  thirdWeatherIcon.setAttribute("alt", response.data.list[3].weather[0].main);
-  let fourthDayMax = document.querySelector("#fourthMax");
-  let fourthDayMin = document.querySelector("#fourthMin");
-  let fourthWeatherIconCode = response.data.list[4].weather[0].icon;
-  let fourthWeatherIcon = document.querySelector("#fourthWeatherIcon");
-  fourthDayMax.innerHTML = "Max: " + Math.round(response.data.list[4].main.temp_max);
-  fourthDayMin.innerHTML = "Min: " + Math.round(response.data.list[4].main.temp_min);
+  thirdWeatherIcon.setAttribute("alt", response.data.daily[2].weather[0].description);
+    
+  fourthDayCelsiusTempMax = (response.data.daily[3].temp.max);
+  fourthDayCelsiusTempMin = (response.data.daily[3].temp.min);
+  fourthDayMaxElement.innerHTML = "Max: " + Math.round(fourthDayCelsiusTempMax);
+  fourthDayMinElement.innerHTML = "Min: " + Math.round(fourthDayCelsiusTempMin);
   fourthWeatherIcon.setAttribute("src",`http://openweathermap.org/img/wn/${fourthWeatherIconCode}@2x.png`);
-  fourthWeatherIcon.setAttribute("alt", response.data.list[4].weather[0].main);
-  let fifthDayMax = document.querySelector("#fifthMax");
-  let fifthDayMin = document.querySelector("#fifthMin");
-  let fifthWeatherIconCode = response.data.list[5].weather[0].icon;
-  let fifthWeatherIcon = document.querySelector("#fifthWeatherIcon");
-  fifthDayMax.innerHTML = "Max: " + Math.round(response.data.list[5].main.temp_max);
-  fifthDayMin.innerHTML = "Min: " + Math.round(response.data.list[5].main.temp_min);
+  fourthWeatherIcon.setAttribute("alt", response.data.daily[3].weather[0].description);
+   
+  fifthDayCelsiusTempMax = (response.data.daily[4].temp.max);
+  fifthDayCelsiusTempMin = (response.data.daily[4].temp.min);
+  fifthDayMaxElement.innerHTML = "Max: " + Math.round(fifthDayCelsiusTempMax);
+  fifthDayMinElement.innerHTML = "Min: " + Math.round(fifthDayCelsiusTempMin);
   fifthWeatherIcon.setAttribute("src",`http://openweathermap.org/img/wn/${fifthWeatherIconCode}@2x.png`);
-  fifthWeatherIcon.setAttribute("alt", response.data.list[5].weather[0].main);
+  fifthWeatherIcon.setAttribute("alt", response.data.daily[4].weather[0].description);
+  
   tomorrow.innerHTML = days[now.getDay() + 1] + ` ` + (now.getDate() + 1);
   secondDay.innerHTML = days[now.getDay() + 2] + ` ` + (now.getDate() + 2);
   thirdDay.innerHTML = days[now.getDay() + 3] + ` ` + (now.getDate() + 3);
@@ -177,36 +196,72 @@ function showForecast(response) {
   fifthDay.innerHTML = days[now.getDay() + 5] + ` ` + (now.getDate() + 5);
 }
 
+
 function convertFahrenheitTemperature(event) {
   event.preventDefault();
-  let currentTemperature = document.querySelector("#currentTemperature");
-  let currentTempMaxElement = document.querySelector("#currentTempMax");
-  let currentTempMinElement = document.querySelector("#currentTempMin");
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = celsiusTemperature * 1.8 + 32;
   let fahrenheitTempMax = celsiusCurrentTempMax * 1.8 + 32;
   let fahrenheitTempMin = celsiusCurrentTempMin * 1.8 + 32;
-  currentTemperature.innerHTML = Math.round(fahrenheitTemperature);
+  let firstDayfahrenheitTempMax = firstDayCelsiusTempMax * 1.8 + 32;
+  let firstDayfahrenheitTempMin = firstDayCelsiusTempMin * 1.8 + 32;
+  let secondDayFahrenheitTempMax = secondDayCelsiusTempMax * 1.8 + 32;
+  let secondDayFahrenheitTempMin = secondDayCelsiusTempMin * 1.8 + 32;
+  let thirdDayfahrenheitTempMax = thirdDayCelsiusTempMax * 1.8 + 32;
+  let thirdDayfahrenheitTempMin = thirdDayCelsiusTempMin * 1.8 + 32;
+  let fourthDayFahrenheitTempMax = fourthDayCelsiusTempMax * 1.8 + 32;
+  let fourthDayFahrenheitTempMin = fourthDayCelsiusTempMin * 1.8 + 32;  
+  let fifthDayFahrenheitTempMax = fifthDayCelsiusTempMax * 1.8 + 32;
+  let fifthDayFahrenheitTempMin = fifthDayCelsiusTempMin * 1.8 + 32;
+
+  currentTempElement.innerHTML = Math.round(fahrenheitTemperature);
   currentTempMaxElement.innerHTML = "Max:" + Math.round(fahrenheitTempMax);
   currentTempMinElement.innerHTML = "Min:"  + Math.round(fahrenheitTempMin);
+  firstDayMaxElement.innerHTML = "Max: " + Math.round(firstDayfahrenheitTempMax);
+  firstDayMinElement.innerHTML = "Min: " + Math.round(firstDayfahrenheitTempMin);
+  secondDayMaxElement.innerHTML = "Max: " + Math.round(secondDayFahrenheitTempMax);
+  secondDayMinElement.innerHTML = "Min: " + Math.round(secondDayFahrenheitTempMin);
+  thirdDayMaxElement.innerHTML = "Max: " + Math.round(thirdDayfahrenheitTempMax);
+  thirdDayMinElement.innerHTML = "Min: " + Math.round(thirdDayfahrenheitTempMin);
+  fourthDayMaxElement.innerHTML = "Max: " + Math.round(fourthDayFahrenheitTempMax);
+  fourthDayMinElement.innerHTML = "Min: " + Math.round(fourthDayFahrenheitTempMin);
+  fifthDayMaxElement.innerHTML = "Max: " + Math.round(fifthDayFahrenheitTempMax);
+  fifthDayMinElement.innerHTML = "Min: " + Math.round(fifthDayFahrenheitTempMin);
 }
 
 function convertCelsiusTemperature(event) {
   event.preventDefault();
-  let currentTemperature = document.querySelector("#currentTemperature");
-  let currentTempMaxElement = document.querySelector("#currentTempMax");
-  let currentTempMinElement = document.querySelector("#currentTempMin");
   fahrenheitLink.classList.remove("active");
   celsiusLink.classList.add("active");
-  currentTemperature.innerHTML = Math.round(celsiusTemperature);
+  currentTempElement.innerHTML = Math.round(celsiusTemperature);
   currentTempMaxElement.innerHTML = "Max:" + Math.round(celsiusCurrentTempMax);
   currentTempMinElement.innerHTML = "Min:"  + Math.round(celsiusCurrentTempMin);
+  firstDayMaxElement.innerHTML = "Max: " + Math.round(firstDayCelsiusTempMax);
+  firstDayMinElement.innerHTML = "Min: " + Math.round(firstDayCelsiusTempMin);
+  secondDayMaxElement.innerHTML = "Max: " + Math.round(secondDayCelsiusTempMax);
+  secondDayMinElement.innerHTML = "Min: " + Math.round(secondDayCelsiusTempMin);
+  thirdDayMaxElement.innerHTML = "Max: " + Math.round(thirdDayCelsiusTempMax);
+  thirdDayMinElement.innerHTML = "Min: " + Math.round(thirdDayCelsiusTempMin);
+  fourthDayMaxElement.innerHTML = "Max: " + Math.round(fourthDayCelsiusTempMax);
+  fourthDayMinElement.innerHTML = "Min: " + Math.round(fourthDayCelsiusTempMin);
+  fifthDayMaxElement.innerHTML = "Max: " + Math.round(fifthDayCelsiusTempMax);
+  fifthDayMinElement.innerHTML = "Min: " + Math.round(fifthDayCelsiusTempMin);
 }
 
 let celsiusTemperature = null;
 let celsiusCurrentTempMax = null;
 let celsiusCurrentTempMin = null;
+let firstDayCelsiusTempMax = null;
+let firstDayCelsiusTempMin = null;
+let secondDayCelsiusTempMax = null;
+let secondDayCelsiusTempMin = null;
+let thirdDayCelsiusTempMax = null;
+let thirdDayCelsiusTempMin = null;
+let fourthDayCelsiusTempMax = null;
+let fourthDayCelsiusTempMin = null;
+let fifthDayCelsiusTempMax = null;
+let fifthDayCelsiusTempMin = null;
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertFahrenheitTemperature);
